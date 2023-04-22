14c84ec7036a42920865c461c351f8f3b5411848
Kieren Enright <kierenenright27@gmail.com>
2023-04-22 12:43:40 -0700

    Posts page displays all available posts from API with information about them

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,12 +1,48 @@
-import React from "react";
-import { Route, Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { fetchFromAPI } from "../api";
 
-const Posts = () => {
+const Posts = ({token}) => {
+    const [posts, setPosts] = useState([]);
+
+    const fetchPosts = async () => {
+        const data = await fetchFromAPI({
+            endpoint: "posts",
+            token
+        })
+
+        if (data?.posts) {
+            setPosts(data.posts);
+        }
+    }
+
+    useEffect(() => {
+        fetchPosts();
+    }, [token]);
+
+
     return (
         <>
             <h1 className="page-title">Posts</h1>
+            <div>
+                {
+                    posts 
+                    ? posts.map(
+                        ({_id, isAuthor, author, location, desription, price, title}, idx) => (
+                            <div className="posts" key={_id ?? idx}>
+                                <h5>{title}</h5>
+                                <h5>{price}</h5>
+                                <h5>{location}</h5>
+                                <p>{desription}</p>
+                                {isAuthor
+                                    ? <small>Created By You</small>
+                                    : <small>Created By {author.username}</small>}
+                            </div> 
+                        )
+                    ) : <strong>No Posts are available</strong>
+                }
+            </div>
         </>
-    )
+    );
 }
 
 export default Posts;
