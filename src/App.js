afc8014615aad9a9ec018b260083e4d69245746e
Kieren Enright <kierenenright27@gmail.com>
2023-04-22 11:14:10 -0700

    moved nav into header tag

--- src/App.js
+++ src/App.js
@@ -10,16 +10,21 @@ import {
 const App = () => {
     return (
         <>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/account">Account</Link>
-                <Link to="/posts">Posts</Link>
-            </nav>
+            <header>
+                <h1 className="logo">Stranger's Things</h1>
+
+                <nav>
+                    <Link to="/">Home</Link>
+                    <Link to="/account">Account</Link>
+                    <Link to="/posts">Posts</Link>
+                </nav>
+
+            </header>
 
             <Route exact path="/">
                 <Home />
             </Route>
-            
+
             <Route path="/account">
                 {/* <Account /> */}
             </Route>
