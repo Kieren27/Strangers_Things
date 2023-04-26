0ee1c6a68b7d8887fd9f37a250165496626642af
Kieren Enright <kierenenright27@gmail.com>
2023-04-26 16:31:58 -0700

    removed export for Home/ added export for Profile

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
-export { default as Home } from './Home';
 export { default as Posts } from './Posts';
 export { default as Account } from './Account';
+export { default as Profile } from './Profile';
