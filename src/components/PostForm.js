401de41b1d0af003a896e554b31b9a2c349fcf20
Kieren Enright <kierenenright27@gmail.com>
2023-04-26 21:21:09 -0700

    add createPost function that should post the correct object and keys to the API

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,14 +1,41 @@
 import React, { useState } from "react";
 import { BrowserHistory } from 'react-router-dom';
+import { fetchFromAPI } from "../api";
 
 const PostForm = ({ token }) => {
     const [title, setTitle] = useState('');
     const [location, setLocation] = useState('');
     const [price, setPrice] = useState('');
     const [description, setDescription] = useState('');
-    const [deliver, setDeliver] = useState('');
+    const [willDeliver, setWilldeliver] = useState('');
 
+    const createPost = async (event) => {
+        event.preventDefault();
 
+        const postData = await fetchFromAPI({
+            endpoint: "post",
+            token,
+            body : {
+                post : {
+                    title,
+                    description,
+                    price,
+                    location,
+                    willDeliver
+                }
+            }
+        })
+        console.log(postData);
+        const { post } = postData;
+        if (post) {
+            setTitle('');
+            setLocation('');
+            setPrice('');
+            setDescription('');
+            setWilldeliver(false);
+        }
+
+    }
 
 }
 
