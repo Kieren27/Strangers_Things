20f1f156749be41b5c24d113781644aa2cd7d476
Kieren Enright <kierenenright27@gmail.com>
2023-04-29 19:55:39 -0700

    filter though posts via title or location

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import PostDetails from "./PostDetails";
 
 const Posts = ({ posts, fetchPosts, token }) => {
+    const [searchValue, setSearchValue] = useState('');
+    const [filteredPosts, setFilteredPosts] = useState(posts);
 
     const onDelete = async () => {
         await fetchPosts();
+    }
+
+    const handleSearch = (event) => {
+        const substring = event.target.value;
+        setSearchValue(substring);
+
+        const filteredPosts = posts
+            .filter(post =>
+                post.title.toLowerCase().includes(substring.toLowerCase().trim()) ||
+                post.location.toLowerCase().includes(substring.toLowerCase().trim()));
+
+        setFilteredPosts(filteredPosts);
     }
 
     return (
@@ -22,20 +36,31 @@ const Posts = ({ posts, fetchPosts, token }) => {
 
             }
 
+            <div className="search-bar">
+                <input
+                    id="search-box"
+                    type="text"
+                    name="search"
+                    value={searchValue}
+                    onChange={handleSearch}
+                />
+            </div>
+
             <div id="post-div">
-                {posts.map(
-                    (posts, idx) => (
-                        <PostDetails 
-                        key={posts._id ?? idx}
-                        posts={posts}
-                        token={token}
-                        onDelete={onDelete}>
-                        <button>
-                            <Link to={`/posts/${posts._id}`} className="link">See Details</Link>
-                        </button>
-                        </PostDetails>
-                    )
-                )}
+                {filteredPosts.length ?
+                    filteredPosts.map(
+                        (posts, idx) => (
+                            <PostDetails
+                                key={posts._id ?? idx}
+                                posts={posts}
+                                token={token}
+                                onDelete={onDelete}>
+                                <button>
+                                    <Link to={`/posts/${posts._id}`} className="link">See Details</Link>
+                                </button>
+                            </PostDetails>
+                        )
+                    ) : <div>No Posts Found</div>}
             </div>
         </>
     );
